46d3321ef3f112024a9f676e034d85710fc8aa29
Александр Матюхин <drugged_monkey@outlook.com>
2022-12-19 21:58:58 +0400

    add server on node + express

---
+++ server/src/app.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from "express";
+import path from "path";
+import url from "url";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app: Express = express();
+const PORT: string = process.env.PORT || "8080";
+
+app.use(express.static(path.join(__dirname, "../../client/public")));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.get("*", (req: Request, res: Response, next: any) => {
+  res.sendFile(path.join(__dirname, "../../client/public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at https://localhost:${PORT}`);
+});
