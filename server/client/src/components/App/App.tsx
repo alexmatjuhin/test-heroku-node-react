985128868ca8714840a02e822177777401852b4c
Александр Матюхин <drugged_monkey@outlook.com>
2022-12-20 14:55:09 +0400

    fix images

--- server/client/src/components/App/App.tsx
+++ server/client/src/components/App/App.tsx
@@ -3,15 +3,21 @@ import styles from "./App.scss";
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer";
 
-//import background from "../../assets/images/background.png";
+import background from "../../assets/images/background.png";
 
 const App = () => {
-    console.log(styles);
+    const biStyle = {
+        backgroundImage: `url(${background})`,
+        backgroundRepeat  : 'no-repeat',
+        backgroundPosition: 'center'
+    };
+
+    console.log(biStyle);
 
     return (
-        <div className={styles.app}>
+        <div className={styles.app} style={biStyle} >
             <Header />
-            <div style={{ backgroundColor: "green" }} className={styles.content} >
+            <div className={styles.content} >
                 <h2>Main Content</h2>
             </div>
             <Footer />
